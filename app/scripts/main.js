9804c01bbdeebad677a79c06e6caa06d96094ba0
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-05-08 19:11:33 -0400

    Streetview via Shareabouts.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,16 +4,16 @@ var Shareabouts = Shareabouts || {};
 
 (function(NS, $) {
 
-var $map = $('.shareabouts-streetview-map'),
-    map = L.mapbox.map($map.get(0), 'examples.map-9ijuk24y')
-      .setView([40.719941, -73.996010], 14),
+  // var $map = $('.shareabouts-streetview-map'),
+  //     map = L.mapbox.map($map.get(0), 'examples.map-9ijuk24y')
+  //       .setView([40.719941, -73.996010], 14),
 
-      intersectionTiles = L.mapbox.tileLayer('openplans.nyc-intersections').addTo(map),
-      intersectionGridLayer = L.mapbox.gridLayer('openplans.nyc-intersections').addTo(map);
+  //     intersectionTiles = L.mapbox.tileLayer('openplans.nyc-intersections').addTo(map),
+  //     intersectionGridLayer = L.mapbox.gridLayer('openplans.nyc-intersections').addTo(map);
 
-  intersectionGridLayer.on('click', function() {
-    console.log(arguments);
-  });
+  // intersectionGridLayer.on('click', function() {
+  //   console.log(arguments);
+  // });
 
 
 }(Shareabouts, jQuery));
