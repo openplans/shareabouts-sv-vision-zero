998cd1c1b3ce6e0501ac72c788e9b34055ee04a2
Aaron Ogle <atogle@gmail.com>
2014-05-13 11:38:18 -0400

    Fix conflicts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-/*globals jQuery L */
+/*globals jQuery, L, google, Handlebars, Spinner */
 
 var Shareabouts = Shareabouts || {};
 
@@ -15,178 +15,219 @@ var Shareabouts = Shareabouts || {};
   //   console.log(arguments);
   // });
 
-      Shareabouts = Shareabouts || {};
+  NS.smallSpinnerOptions = {
+    lines: 13, length: 0, width: 3, radius: 10, corners: 1, rotate: 0,
+    direction: 1, color: '#000', speed: 1, trail: 60, shadow: false,
+    hwaccel: false, className: 'spinner', zIndex: 2e9, top: 'auto',
+    left: 'auto'
+  };
 
-      Shareabouts.smallSpinnerOptions = {
-        lines: 13, length: 0, width: 3, radius: 10, corners: 1, rotate: 0,
-        direction: 1, color: '#000', speed: 1, trail: 60, shadow: false,
-        hwaccel: false, className: 'spinner', zIndex: 2e9, top: 'auto',
-        left: 'auto'
-      };
+  NS.Config = {
+    placeTypes: {
+      'notime': { label: 'Not Enough Time to Cross' },
+      'doublepark': { label: 'Double Parking' },
+      'longwait': { label: 'Long Wait to Cross' },
+      'redlight': { label: 'Red Light Running' },
+      'jaywalking': { label: 'Jaywalking' },
+      'visibility': { label: 'Poor Visibility' },
+      'speeding': { label: 'Speeding' },
+      'longcross': { label: 'Long Distance to Cross' },
+      'yield': { label: 'Failure to Yield' },
+      'bike': { label: 'Cyclist Behavior' },
+      'other': { label: 'Other' }
+    }
+  };
 
-      Shareabouts.Config = {
-        placeTypes: {
-          'notime': { label: 'Not Enough Time to Cross' },
-          'doublepark': { label: 'Double Parking' },
-          'longwait': { label: 'Long Wait to Cross' },
-          'redlight': { label: 'Red Light Running' },
-          'jaywalking': { label: 'Jaywalking' },
-          'visibility': { label: 'Poor Visibility' },
-          'speeding': { label: 'Speeding' },
-          'longcross': { label: 'Long Distance to Cross' },
-          'yield': { label: 'Failure to Yield' },
-          'bike': { label: 'Cyclist Behavior' },
-          'other': { label: 'Other' }
-        }
-      }
-
-      function loadStreetView(lat, lng) {
-        var sa = new Shareabouts.StreetView({
-          el: '.shareabouts-streetview',
-          map: {
-            center: [lat, lng],
-            maxDistance: '100m'
+  function loadStreetView(lat, lng) {
+    var containerSelector = '.shareabouts-streetview';
+    var sa = new NS.StreetView({
+      el: containerSelector,
+      map: {
+        center: [lat, lng],
+        maxDistance: '100m'
+      },
+      placeStyles: [
+        {
+          condition: '"{{location_type}}" == "doublepark"',
+          icon: {
+            url: 'styles/images/markers/marker-doublepark.png'
           },
-          placeStyles: [
-            {
-              condition: '"{{location_type}}" == "doublepark"',
-              icon: {
-                url: 'styles/images/markers/marker-doublepark.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-doublepark.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "jaywalking"',
-              icon: {
-                url: 'styles/images/markers/marker-jaywalking.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-jaywalking.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "longcross"',
-              icon: {
-                url: 'styles/images/markers/marker-longcross.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-longcross.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "longwait"',
-              icon: {
-                url: 'styles/images/markers/marker-longwait.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-longwait.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "notime"',
-              icon: {
-                url: 'styles/images/markers/marker-notime.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-notime.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "redlight"',
-              icon: {
-                url: 'styles/images/markers/marker-redlight.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-redlight.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "speeding"',
-              icon: {
-                url: 'styles/images/markers/marker-speeding.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-speeding.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "visibility"',
-              icon: {
-                url: 'styles/images/markers/marker-visibility.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-visibility.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "yield"',
-              icon: {
-                url: 'styles/images/markers/marker-yield.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-yield.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "bike"',
-              icon: {
-                url: 'styles/images/markers/marker-bike.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-bike.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: '"{{location_type}}" == "other"',
-              icon: {
-                url: 'styles/images/markers/marker-unknown.png'
-              },
-              focusIcon: {
-                url: 'styles/images/markers/marker-unknown.png',
-                scaledSize: new google.maps.Size(81, 120)
-              }
-            },
-            {
-              condition: 'true',
-              newIcon: {
-                url: 'styles/images/marker-plus-shadowed.png',
-                anchor: new google.maps.Point(42, 80)
-              }
-            }
-          ],
+          focusIcon: {
+            url: 'styles/images/markers/marker-doublepark.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "jaywalking"',
+          icon: {
+            url: 'styles/images/markers/marker-jaywalking.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-jaywalking.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "longcross"',
+          icon: {
+            url: 'styles/images/markers/marker-longcross.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-longcross.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "longwait"',
+          icon: {
+            url: 'styles/images/markers/marker-longwait.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-longwait.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "notime"',
+          icon: {
+            url: 'styles/images/markers/marker-notime.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-notime.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "redlight"',
+          icon: {
+            url: 'styles/images/markers/marker-redlight.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-redlight.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "speeding"',
+          icon: {
+            url: 'styles/images/markers/marker-speeding.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-speeding.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "visibility"',
+          icon: {
+            url: 'styles/images/markers/marker-visibility.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-visibility.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "yield"',
+          icon: {
+            url: 'styles/images/markers/marker-yield.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-yield.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "bike"',
+          icon: {
+            url: 'styles/images/markers/marker-bike.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-bike.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: '"{{location_type}}" == "other"',
+          icon: {
+            url: 'styles/images/markers/marker-unknown.png'
+          },
+          focusIcon: {
+            url: 'styles/images/markers/marker-unknown.png',
+            scaledSize: new google.maps.Size(81, 120)
+          }
+        },
+        {
+          condition: 'true',
+          newIcon: {
+            url: 'styles/images/marker-plus-shadowed.png',
+            anchor: new google.maps.Point(42, 80)
+          }
+        }
+      ],
 
-          datasetUrl: 'http://data.shareabouts.org/api/v2/nycdot/datasets/vz-dev/places',
-          addButtonLabel: 'Share an Issue',
-          maxDistance: 25,
+      datasetUrl: 'http://data.shareabouts.org/api/v2/nycdot/datasets/vz-dev/places',
+      addButtonLabel: 'Share an Issue',
+      maxDistance: 25,
 
-          // These are template functions that expect geojson.
-          templates: {
-            'place-summary': Handlebars.compile($('#place-summary').html()),
-            'place-detail': Handlebars.compile($('#place-detail').html()),
-            'place-form': Handlebars.compile($('#place-form').html()),
-            'place-survey': Handlebars.compile($('#place-survey').html()),
-            'place-survey-item': Handlebars.compile($('#place-survey-item').html())
+      // These are template functions that expect geojson.
+      templates: {
+        'place-summary': Handlebars.compile($('#place-summary').html()),
+        'place-detail': Handlebars.compile($('#place-detail').html()),
+        'place-form': Handlebars.compile($('#place-form').html()),
+        'place-survey': Handlebars.compile($('#place-survey').html()),
+        'place-survey-item': Handlebars.compile($('#place-survey-item').html())
+      }
+    });
+
+    $(sa).on('showplacesurvey', function(evt, view) {
+      var spinner = new Spinner(NS.smallSpinnerOptions).spin(view.$('.form-spinner')[0]);
+    });
+
+    // Show the streetview container
+    $(containerSelector).addClass('active')
+      .siblings('.shareabouts-location-map-container').removeClass('active');
+  }
+
+  function initMap() {
+    var map = new google.maps.Map($('.shareabouts-location-map').get(0),
+          {center: new google.maps.LatLng(40.7210690835, -73.9981985092), zoom: 14}),
+        intersectionLayer = new google.maps.FusionTablesLayer({
+          suppressInfoWindows: true,
+          query: {
+            select: '*',
+            from: '1LGooNPVO91c5Qu4Bh27bwisYcHPVcbztDBQhhSvs'
           }
         });
 
-        $(sa).on('showplacesurvey', function(evt, view) {
-          spinner = new Spinner(Shareabouts.smallSpinnerOptions).spin(view.$('.form-spinner')[0]);
-        })
-      }
+    var crashDataMapType = new google.maps.ImageMapType({
+      getTileUrl: function(coord, zoom) {
+        function getRandomInt (min, max) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 
-      loadStreetView(40.7210690835, -73.9981985092);
+        var subdomains = ['a', 'b', 'c', 'd'];
+
+        return ['http://', subdomains[getRandomInt(0, 3)], '.tiles.mapbox.com/v3/openplans.i7opnbif/',
+            zoom, '/', coord.x, '/', coord.y, '.png'].join('');
+      },
+      tileSize: new google.maps.Size(256, 256)
+    });
+
+    map.overlayMapTypes.push(crashDataMapType);
+
+
+
+    intersectionLayer.setMap(map);
+
+    // Show street view when the user clicks on an intersection
+    google.maps.event.addListener(intersectionLayer, 'click', function(evt) {
+      var latLng = evt.latLng;
+      loadStreetView(latLng.lat(), latLng.lng());
+    });
+
+
+  }
 
   $(document).delegate('.place-type-selector','click',function(evt){
     var $target = $(evt.currentTarget),
@@ -224,21 +265,21 @@ var Shareabouts = Shareabouts || {};
     }
   });
 
+  $(function() {
+    initMap();
 
-      // Make a new map, and recenter when we click.
-      map = L.map($('.shareabouts-location-map').get(0), {center: [40.7210690835, -73.9981985092], zoom: 14});
-      layerOptions = {
-        url: 'http://{s}.tiles.mapbox.com/v3/openplans.map-dmar86ym/{z}/{x}/{y}.png',
-        attribution: '&copy; OpenStreetMap contributors, CC-BY-SA. <a href="http://mapbox.com/about/maps" target="_blank">Terms &amp; Feedback</a>'
-      };
-      L.tileLayer(layerOptions.url, layerOptions).addTo(map);
+    // var layerOptions = {
+    //   url: 'http://{s}.tiles.mapbox.com/v3/openplans.map-dmar86ym/{z}/{x}/{y}.png',
+    //   attribution: '&copy; OpenStreetMap contributors, CC-BY-SA. <a href="http://mapbox.com/about/maps" target="_blank">Terms &amp; Feedback</a>'
+    // };
+    // L.tileLayer(layerOptions.url, layerOptions).addTo(map);
 
-      map.on('click', function(evt) {
-        var lat = evt.latlng.lat,
-            lng = evt.latlng.lng;
+    // map.on('click', function(evt) {
+    //   var lat = evt.latlng.lat,
+    //       lng = evt.latlng.lng;
 
-        loadStreetView(lat, lng);
-      })
+    //   loadStreetView(lat, lng);
+    // });
 
-
+  });
 }(Shareabouts, jQuery));
