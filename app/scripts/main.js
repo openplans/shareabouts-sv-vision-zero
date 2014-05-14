474276e6560873f31019252e7d023ef86dcd8395
Aaron Ogle <atogle@gmail.com>
2014-05-14 09:36:30 -0400

    Update and consolidate the js dependencies.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,9 @@
-/*globals jQuery, L, google, Handlebars, Spinner, wax */
+/*globals jQuery, L, google, Handlebars, Spinner, wax, Swag */
 
 var Shareabouts = Shareabouts || {};
 
 (function(NS, $) {
-
-  // var $map = $('.shareabouts-streetview-map'),
-  //     map = L.mapbox.map($map.get(0), 'examples.map-9ijuk24y')
-  //       .setView([40.719941, -73.996010], 14),
-
-  //     intersectionTiles = L.mapbox.tileLayer('openplans.nyc-intersections').addTo(map),
-  //     intersectionGridLayer = L.mapbox.gridLayer('openplans.nyc-intersections').addTo(map);
-
-  // intersectionGridLayer.on('click', function() {
-  //   console.log(arguments);
-  // });
+  Swag.registerHelpers();
 
   NS.smallSpinnerOptions = {
     lines: 13, length: 0, width: 3, radius: 10, corners: 1, rotate: 0,
