21244002779f5b3f7c70d6438b5c0b3f26f21121
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2014-05-16 15:16:58 -0400

    Add intersection data files

---
+++ app/data/generate_intersection_files.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+import json
+from os.path import join as pathjoin, dirname, exists
+from multiprocessing import Pool
+
+BASEDIR = dirname(__file__)
+
+
+def write_intersection_file(data):
+    nodeid = data['NodeID_1']
+
+    # Split the files into separate folders so that there's not more than 1000
+    # or so files per folder.
+    path = pathjoin(BASEDIR, *(digit for digit in nodeid[:4]))
+
+    if not exists(path):
+        os.makedirs(path)
+
+    with open(pathjoin(path, nodeid + '.json'), 'w') as intersection_file:
+        json.dump(data, intersection_file, indent=2)
+
+
+if __name__ == '__main__':
+    with open(pathjoin(BASEDIR, 'intersection_data.json')) as intersections_file:
+        data = json.load(intersections_file)
+        if isinstance(data, dict):
+            data_iter = data.values()
+        else:
+            data_iter = data
+
+    pool = Pool(processes=4)
+    pool.map(write_intersection_file, data_iter)
