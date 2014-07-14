6fc543d578465eae23229faf3860106981bcfa5b
Aaron Ogle <atogle@gmail.com>
2014-07-14 14:23:47 -0400

    Update the dataset url to be production.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -291,7 +291,7 @@ var Shareabouts = Shareabouts || {};
       'bike': '#00aeef',
     },
     mapStyle: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#ece2d9"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"off"}]}],
-    datasetUrl: 'http://data.shareabouts.org/api/v2/admin/datasets/vz-dev/places'
+    datasetUrl: 'http://data.shareabouts.org/api/v2/nycdot/datasets/vz/places'
   };
 
   NS.Router = Backbone.Router.extend({
